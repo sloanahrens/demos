a94139f74a1cae974c8abdd855d98fc209dcb450
Sloan Ahrens <sloan.ahrens@gmail.com>
2014-12-01 12:04:01 -0600

    Initial import from app-template.

---
+++ fabfile/servers.py
@@ -0,0 +1,342 @@
+#!/usr/bin/env python
+
+"""
+Commands work with servers. (Hiss, boo.)
+"""
+
+import copy
+
+from fabric.api import local, put, settings, require, run, sudo, task
+from fabric.state import env
+from jinja2 import Template
+
+import app_config
+
+
+# sa added
+##########
+
+"""
+Fix Nginx
+"""
+
+@task
+def fix_nginx():
+    """
+    Fixes issue with nginx conf; run after deploy_confs
+    """
+    require('settings', provided_by=['production', 'staging'])
+    require('branch', provided_by=['stable', 'master', 'branch'])
+
+    if not app_config.DEPLOY_TO_SERVERS:
+        print 'You must set DEPLOY_TO_SERVERS = True in your app_config.py before setting up the servers.'
+
+    run('sudo rm -rf /etc/nginx/sites-enabled/default')
+    run('sudo ln -s /etc/nginx/sites-available/%s.nginx.conf /etc/nginx/sites-enabled/%s.nginx.conf' % (app_config.PROJECT_FILENAME, app_config.PROJECT_FILENAME))
+    run('sudo service nginx restart')
+
+
+"""
+Prepare
+"""
+
+@task
+def prepare():
+    """
+    Prepares VM for setup
+    """
+    require('settings', provided_by=['production', 'staging'])
+    require('branch', provided_by=['stable', 'master', 'branch'])
+
+    if not app_config.DEPLOY_TO_SERVERS:
+        print 'You must set DEPLOY_TO_SERVERS = True in your app_config.py before setting up the servers.'
+
+        return
+    run('sudo chown ubuntu /var -R; sudo chown ubuntu /tmp -R')
+    run('sudo apt-get -y update')
+    run('sudo apt-get -y install software-properties-common')
+    run('sudo apt-get -y install python-software-properties python g++ make')
+    run('sudo apt-get -y install python-dev')
+    run('sudo apt-get -y install python-setuptools')
+    run('sudo easy_install pip')
+    run('sudo pip install virtualenv virtualenvwrapper')
+    run('sudo pip install uwsgi')
+    run('sudo apt-get -y install git')
+    run('sudo add-apt-repository -y ppa:chris-lea/node.js')
+    run('sudo apt-get -y update')
+    run('sudo apt-get -y install nodejs')
+    run('sudo apt-get -y install nginx')
+    run('sudo apt-get -y install curl')
+
+
+"""
+Install ES
+"""
+
+@task
+def install_es():
+    """
+    Install Open-JDK 7 and Elasticsearch 1.3.4
+    """
+    require('settings', provided_by=['production', 'staging'])
+    require('branch', provided_by=['stable', 'master', 'branch'])
+
+    if not app_config.DEPLOY_TO_SERVERS:
+        print 'You must set DEPLOY_TO_SERVERS = True in your app_config.py before setting up the servers.'
+
+    run('sudo apt-get -y purge openjdk*')
+    run('sudo apt-get -y install openjdk-7-jdk')
+    run('wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.3.4.tar.gz -O elasticsearch.tar.gz')
+    run('tar -xf elasticsearch.tar.gz')
+    run('rm elasticsearch.tar.gz')
+    run('sudo mv elasticsearch-* elasticsearch')
+    run('sudo mv elasticsearch /usr/local/share')
+    run('curl -L http://github.com/elasticsearch/elasticsearch-servicewrapper/tarball/master | tar -xz')
+    run('sudo mv *servicewrapper*/service /usr/local/share/elasticsearch/bin/')
+    run('rm -Rf *servicewrapper*')
+    run('sudo /usr/local/share/elasticsearch/bin/service/elasticsearch install')
+    run("sudo ln -s 'readlink -f /usr/local/share/elasticsearch/bin/service/elasticsearch' /usr/local/bin/rcelasticsearch")
+    run('sudo service elasticsearch start')
+
+##########
+
+"""
+Setup
+"""
+
+@task
+def setup():
+    """
+    Setup servers for deployment.
+
+    This does not setup services or push to S3. Run deploy() next.
+    """
+    require('settings', provided_by=['production', 'staging'])
+    require('branch', provided_by=['stable', 'master', 'branch'])
+
+    if not app_config.DEPLOY_TO_SERVERS:
+        print 'You must set DEPLOY_TO_SERVERS = True in your app_config.py before setting up the servers.'
+
+        return
+
+    create_directories()
+    create_virtualenv()
+    clone_repo()
+    checkout_latest()
+    install_requirements()
+
+
+def create_directories():
+    """
+    Create server directories.
+    """
+    require('settings', provided_by=['production', 'staging'])
+
+    run('mkdir -p %(SERVER_PROJECT_PATH)s' % app_config.__dict__)
+    run('mkdir -p /var/www/uploads/%(PROJECT_FILENAME)s' % app_config.__dict__)
+
+def create_virtualenv():
+    """
+    Setup a server virtualenv.
+    """
+    require('settings', provided_by=['production', 'staging'])
+
+    run('virtualenv -p %(SERVER_PYTHON)s --no-site-packages %(SERVER_VIRTUALENV_PATH)s' % app_config.__dict__)
+    run('source %(SERVER_VIRTUALENV_PATH)s/bin/activate' % app_config.__dict__)
+
+def clone_repo():
+    """
+    Clone the source repository.
+    """
+    require('settings', provided_by=['production', 'staging'])
+
+    run('git clone %(REPOSITORY_URL)s %(SERVER_REPOSITORY_PATH)s' % app_config.__dict__)
+
+    if app_config.REPOSITORY_ALT_URL:
+        run('git remote add bitbucket %(REPOSITORY_ALT_URL)s' % app_config.__dict__)
+
+@task
+def checkout_latest(remote='origin'):
+    """
+    Checkout the latest source.
+    """
+    require('settings', provided_by=['production', 'staging'])
+    require('branch', provided_by=['stable', 'master', 'branch'])
+
+    run('cd %s; git fetch %s' % (app_config.SERVER_REPOSITORY_PATH, remote))
+    run('cd %s; git checkout %s; git pull %s %s' % (app_config.SERVER_REPOSITORY_PATH, env.branch, remote, env.branch))
+
+@task
+def install_requirements():
+    """
+    Install the latest requirements.
+    """
+    require('settings', provided_by=['production', 'staging'])
+
+    run('%(SERVER_VIRTUALENV_PATH)s/bin/pip install -U -r %(SERVER_REPOSITORY_PATH)s/requirements.txt' % app_config.__dict__)
+    run('cd %(SERVER_REPOSITORY_PATH)s; npm install' % app_config.__dict__) 
+
+@task
+def install_crontab():
+    """
+    Install cron jobs script into cron.d.
+    """
+    require('settings', provided_by=['production', 'staging'])
+
+    sudo('cp %(SERVER_REPOSITORY_PATH)s/crontab /etc/cron.d/%(PROJECT_FILENAME)s' % app_config.__dict__)
+
+@task
+def uninstall_crontab():
+    """
+    Remove a previously install cron jobs script from cron.d
+    """
+    require('settings', provided_by=['production', 'staging'])
+
+    sudo('rm /etc/cron.d/%(PROJECT_FILENAME)s' % app_config.__dict__)
+
+def delete_project():
+    """
+    Remove the project directory. Invoked by shiva.
+    """
+    run('rm -rf %(SERVER_PROJECT_PATH)s' % app_config.__dict__)
+
+"""
+Configuration
+"""
+
+def _get_template_conf_path(service, extension):
+    """
+    Derive the path for a conf template file.
+    """
+    return 'confs/%s.%s' % (service, extension)
+
+def _get_rendered_conf_path(service, extension):
+    """
+    Derive the rendered path for a conf file.
+    """
+    return 'confs/rendered/%s.%s.%s' % (app_config.PROJECT_FILENAME, service, extension)
+
+def _get_installed_conf_path(service, remote_path, extension):
+    """
+    Derive the installed path for a conf file.
+    """
+    return '%s/%s.%s.%s' % (remote_path, app_config.PROJECT_FILENAME, service, extension)
+
+def _get_installed_service_name(service):
+    """
+    Derive the init service name for an installed service.
+    """
+    return '%s.%s' % (app_config.PROJECT_FILENAME, service)
+
+@task
+def render_confs():
+    """
+    Renders server configurations.
+    """
+    require('settings', provided_by=['production', 'staging'])
+
+    with settings(warn_only=True):
+        local('mkdir confs/rendered')
+
+    # Copy the app_config so that when we load the secrets they don't
+    # get exposed to other management commands
+    context = copy.copy(app_config.__dict__)
+    context.update(app_config.get_secrets())
+
+    for service, remote_path, extension in app_config.SERVER_SERVICES:
+        template_path = _get_template_conf_path(service, extension)
+        rendered_path = _get_rendered_conf_path(service, extension)
+
+        with open(template_path,  'r') as read_template:
+
+            with open(rendered_path, 'wb') as write_template:
+                payload = Template(read_template.read())
+                write_template.write(payload.render(**context))
+
+@task
+def deploy_confs():
+    """
+    Deploys rendered server configurations to the specified server.
+    This will reload nginx and the appropriate uwsgi config.
+    """
+    require('settings', provided_by=['production', 'staging'])
+
+    render_confs()
+
+    with settings(warn_only=True):
+        for service, remote_path, extension in app_config.SERVER_SERVICES:
+            rendered_path = _get_rendered_conf_path(service, extension)
+            installed_path = _get_installed_conf_path(service, remote_path, extension)
+
+            # another OSX difference, I guess
+            #a = local('md5 -q %s' % rendered_path, capture=True)
+            a = local('md5sum %s' % rendered_path, capture=True).split()[0]
+            b = run('md5sum %s' % installed_path).split()[0]
+
+            if a != b:
+                print 'Updating %s' % installed_path
+                put(rendered_path, installed_path, use_sudo=True)
+
+                if service == 'nginx':
+                    sudo('service nginx reload')
+                elif service == 'uwsgi':
+                    service_name = _get_installed_service_name(service)
+                    sudo('initctl reload-configuration')
+                    sudo('service %s restart' % service_name)
+                elif service == 'app':
+                    run('touch %s' % app_config.UWSGI_SOCKET_PATH)
+                    sudo('chmod 644 %s' % app_config.UWSGI_SOCKET_PATH)
+                    sudo('chown www-data:www-data %s' % app_config.UWSGI_SOCKET_PATH)
+
+                    sudo('touch %s' % app_config.UWSGI_LOG_PATH)
+                    sudo('chmod 644 %s' % app_config.UWSGI_LOG_PATH)
+                    sudo('chown ubuntu:ubuntu %s' % app_config.UWSGI_LOG_PATH)
+
+                    sudo('touch %s' % app_config.APP_LOG_PATH)
+                    sudo('chmod 644 %s' % app_config.APP_LOG_PATH)
+                    sudo('chown ubuntu:ubuntu %s' % app_config.APP_LOG_PATH)
+            else:
+                print '%s has not changed' % rendered_path
+
+@task
+def nuke_confs():
+    """
+    DESTROYS rendered server configurations from the specified server.
+    This will reload nginx and stop the uwsgi config.
+    """
+    require('settings', provided_by=['production', 'staging'])
+
+    for service, remote_path, extension in app_config.SERVER_SERVICES:
+        with settings(warn_only=True):
+            installed_path = _get_installed_conf_path(service, remote_path, extension)
+
+            sudo('rm -f %s' % installed_path)
+
+            if service == 'nginx':
+                sudo('service nginx reload')
+            elif service == 'uwsgi':
+                service_name = _get_installed_service_name(service)
+                sudo('service %s stop' % service_name)
+                sudo('initctl reload-configuration')
+            elif service == 'app':
+                sudo('rm %s' % app_config.UWSGI_SOCKET_PATH)
+                sudo('rm %s' % app_config.UWSGI_LOG_PATH)
+                sudo('rm %s' % app_config.APP_LOG_PATH)
+
+"""
+Fabcasting
+"""
+
+@task
+def fabcast(command):
+    """
+    Actually run specified commands on the server specified
+    by staging() or production().
+    """
+    require('settings', provided_by=['production', 'staging'])
+
+    if not app_config.DEPLOY_TO_SERVERS:
+        print 'You must set DEPLOY_TO_SERVERS = True in your app_config.py and setup a server before fabcasting..'
+
+    run('cd %s && bash run_on_server.sh fab %s $DEPLOYMENT_TARGET %s' % (app_config.SERVER_REPOSITORY_PATH, env.branch, command))
+
